7229c4839868430b40df828dca850c88915a3230
13golds <13gaudi@gmail.com>
2024-12-24 13:32:10 +0300

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,1 +1,19 @@
+async function main() {
+    const [deployer] = await ethers.getSigners();
+
+    console.log("Deploying contracts with the account:", deployer.address);
+
+    const RECYC = await ethers.getContractFactory("RECYC");
+    const feeCollectorAddress = "0x73731b3605a01a60308A57d4F8B0BDFfA8691eAD"; 
+    const contract = await RECYC.deploy(feeCollectorAddress);
+
+    console.log("RECYC deployed to:", contract.address);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
 
