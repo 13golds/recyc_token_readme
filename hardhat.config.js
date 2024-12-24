5948d11c490d5bba940e5d6c6ff7a57b106ad7d5
13golds <13gaudi@gmail.com>
2024-12-25 01:10:29 +0300

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,12 @@
-require('@nomiclabs/hardhat-waffle');
+require('@nomiclabs/hardhat-ethers');
+require('dotenv').config();
 
 module.exports = {
-  solidity: "0.8.0",
+  solidity: "0.8.0",  // Версия Solidity
+  networks: {
+    bsc: {
+      url: process.env.BSC_URL, // URL для BSC
+      accounts: [`0x${process.env.PRIVATE_KEY}`], // Ваш приватный ключ из .env
+    },
+  },
 };
