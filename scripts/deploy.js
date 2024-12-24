18db605d8c09f348ca4a7c75a401510a5e51f803
13golds <13gaudi@gmail.com>
2024-12-25 01:16:36 +0300

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,19 +1,19 @@
 async function main() {
-    const [deployer] = await ethers.getSigners();
+  const [deployer] = await ethers.getSigners();
+  console.log("Deploying contracts with the account:", deployer.address);
 
-    console.log("Deploying contracts with the account:", deployer.address);
+  const RecycToken = await ethers.getContractFactory("RecycToken");
+  const recycToken = await RecycToken.deploy();
+  console.log("RecycToken deployed to:", recycToken.0x5462f09252a86ECEcefd35Ab974E61943530593D);
 
-    const RECYC = await ethers.getContractFactory("RECYC");
-    const feeCollectorAddress = "0x73731b3605a01a60308A57d4F8B0BDFfA8691eAD"; 
-    const contract = await RECYC.deploy(feeCollectorAddress);
 
-    console.log("RECYC deployed to:", contract.address);
+  await recycToken.setTransactionFee(1);
+  console.log("Transaction Fee set to:", await recycToken.transactionFeePercentage());
 }
 
 main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-        console.error(error);
-        process.exit(1);
-    });
-
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
